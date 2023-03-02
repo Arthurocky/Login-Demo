edb2e5c625e9b7948bc6c6833b4fbfb67aad5df6
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-02 22:18:11 +0800

    🎉 Initial commit

---
+++ src/test/java/com/loki/Login/LoginDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.loki.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}

6731921219068b56ee41ea41caacded7f3c29130
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-02 22:40:07 +0800

    🚀 Mysql Basic Setup

--- src/main/java/com/loki/Login/LoginDemoApplication.java
+++ src/main/java/com/loki/Login/LoginDemoApplication.java
@@ -1,9 +1,12 @@
 package com.loki.Login;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com/loki/Login/mapper")
 public class LoginDemoApplication {
 
     public static void main(String[] args)

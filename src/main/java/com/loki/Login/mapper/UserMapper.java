5bb3f99bd9ea4c2b2466be298a0e094cd57425ce
Athurocky <arthurocky@user.noreply.gitee.com>
2023-03-02 22:51:24 +0800

    🚀 Add MyBatis-Plus

---
+++ src/main/java/com/loki/Login/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.loki.Login.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.loki.Login.bean.User;
+
+/**
+ * @author Arthurocky
+ */
+public interface UserMapper extends BaseMapper<User> {
+}

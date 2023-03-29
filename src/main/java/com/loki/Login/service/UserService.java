7dc21bc18fc1a51c07f575f4a798fd1082f3726e
Arthurocky <luoqi199904@outlook.com>
2023-03-29 22:31:46 +0800

    ✨ 注册校验

--- src/main/java/com/loki/Login/service/UserService.java
+++ src/main/java/com/loki/Login/service/UserService.java
@@ -2,6 +2,7 @@ package com.loki.Login.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.loki.Login.model.User;
+import org.apache.ibatis.annotations.Param;
 
 /**
 * @author Arthurocky
@@ -9,5 +10,15 @@ import com.loki.Login.model.User;
 * @createDate 2023-03-05 23:07:40
 */
 public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @param userCode 用户编号
+     * @return 新用户 id
+     */
+    long userRegister(@Param("userAccount") String userAccount, @Param("userPassword") String userPassword, @Param("checkPassword") String checkPassword, @Param("userCode") String userCode);
 
 }
